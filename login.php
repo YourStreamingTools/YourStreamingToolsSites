1abc49a81a307249954fa3b053012fe5e7f28af2
SimDifRandomHD <contact@gfaundead.stream>
2023-05-27 21:55:23 +1000

    Create login.php

---
+++ login.php
@@ -0,0 +1,161 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in, if yes then redirect him to dashboard page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: profile.php");
+    exit;
+}
+
+// Include config file
+require_once "db-connect.php";
+
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password, last_login FROM users WHERE username = ?";
+
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set parameters
+            $param_username = $username;
+
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $last_login);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so update last login time and start a new session
+                            $last_login = date('Y-m-d H:i:s');
+                            $sql = "UPDATE users SET last_login = ? WHERE id = ?";
+                            if($stmt = mysqli_prepare($conn, $sql)){
+                                // Bind variables to the prepared statement as parameters
+                                mysqli_stmt_bind_param($stmt, "si", $last_login, $id);
+
+                                // Attempt to execute the prepared statement
+                                if(mysqli_stmt_execute($stmt)){
+                                    // Store data in session variables
+                                    session_start();
+                                    $_SESSION["loggedin"] = true;
+                                    $_SESSION["user_id"] = $id;
+                                    $_SESSION["username"] = $username;                            
+                                    $_SESSION["last_login"] = $last_login;
+
+                                    // Redirect user to dashboard page
+                                    header("location: dashboard.php");
+                                } else{
+                                    echo "Oops! Something went wrong. Please try again later.";
+                                }
+                                mysqli_stmt_close($stmt);
+                            }
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>YourListOnline - Login</title>
+    <link rel="icon" href="https://cdn.yourstreaming.tools/img/logo.png" type="image/png" />
+    <link rel="apple-touch-icon" href="https://cdn.yourstreaming.tools/img/logo.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/style.css">
+    <style type="text/css">
+        body {
+            font: 14px sans-serif;
+        }
+        .wrapper {
+            width: 350px; padding: 20px;
+        }
+            a.popup-link {
+            text-decoration: none;
+            color: black;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="https://yourstreaming.tools/">YourStreamingTools</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="../index.php">Home</a></li>
+                <li class="active"><a href="login.php">Login</a></li>
+                <li><a href="register.php">Sign Up</a></li>
+            </ul>
+            <p class="navbar-text navbar-right">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</p>
+        </div>
+    </nav>
+    <div class="col-md-6">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+                <a href="index.php" class="btn btn-link">Back to Home</a>
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
