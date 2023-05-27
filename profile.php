551ba8036469111b24aa97c2ae06b3f4f83b0a85
SimDifRandomHD <contact@gfaundead.stream>
2023-05-27 22:37:07 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -45,6 +45,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/style.css">
+  <script src="https://cdn.yourstreaming.tools/js/profile.js"></script>
   <style type="text/css">
     body {
       font: 14px sans-serif;
