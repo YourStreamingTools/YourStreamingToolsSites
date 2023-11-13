7972816bb27e04df30a32eb09d6f261290e4d5bf
GFAUnDead <contact@gfaundead.stream>
2023-11-13 15:02:39 +1100

    Update add_command.php

--- rocketwolf/add_command.php
+++ rocketwolf/add_command.php
@@ -3,19 +3,16 @@ $dbFile = "commands.db"; // Path to your SQLite database file
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the form was submitted
-    if (isset($_POST["command_name"]) && !empty($_POST["command_name"]) &&
-        isset($_POST["response"]) && !empty($_POST["response"])) {
+    if (isset($_POST["command_name"]) && !empty($_POST["command_name"])) {
         $commandName = $_POST["command_name"];
-        $response = $_POST["response"];
 
         try {
             $db = new SQLite3($dbFile);
 
-            // Insert the new command and response into the database
-            $query = "INSERT INTO commands (command_name, response) VALUES (:commandName, :response)";
+            // Insert the new command into the database
+            $query = "INSERT INTO commands (command_name) VALUES (:commandName)";
             $stmt = $db->prepare($query);
             $stmt->bindParam(":commandName", $commandName, SQLITE3_TEXT);
-            $stmt->bindParam(":response", $response, SQLITE3_TEXT);
             $stmt->execute();
 
             // Close the database connection
@@ -45,8 +42,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <form method="POST" action="add_command.php">
             <label for="command_name">Command Name:</label>
             <input type="text" id="command_name" name="command_name" required>
-            <label for="response">Response:</label>
-            <textarea id="response" name="response" required class="response"></textarea>
             <button type="submit" class="submit-button">Add Command</button>
         </form>
     </div>
